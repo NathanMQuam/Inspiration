874a2731fb106284818fc3ee1e9ad952b521f6f0
NathanMQuam <nathan.m.quam@gmail.com>
2021-02-28 22:15:53 -0700

    Quotes

--- app/Controllers/QuoteController.js
+++ app/Controllers/QuoteController.js
@@ -1,5 +1,17 @@
 import { ProxyState } from "../AppState.js";
+import { quoteService } from "../Services/QuoteService.js";
+
+function _draw () {
+    document.getElementById( 'quote' ).innerHTML = quoteService.Template
+}
 
 export default class QuoteController {
+    constructor() {
+        ProxyState.on( 'quote', _draw )
+        this.getQuote()
+    }
 
+    getQuote () {
+        quoteService.getQuote()
+    }
 }
--- app/Services/QuoteService.js
+++ app/Services/QuoteService.js
@@ -1,5 +1,25 @@
-class QuoteService {
+import { ProxyState } from "../AppState.js";
+import { sandboxApi } from "./AxiosService.js";
 
+class QuoteService {
+    async getQuote () {
+        try {
+            let res = await sandboxApi.get( 'quotes' )
+            res = res.data
+            ProxyState.quote = { content: res.content, author: res.author }
+            console.log( ProxyState.quote );
+        } catch ( error ) {
+            console.error( error );
+        }
+    }
+
+    get Template () {
+        let qu = ProxyState.quote
+        return /*html*/ `
+            ${qu.content} <br/>
+            <small>${qu.author}</small>
+        `
+    }
 }
 
 export const quoteService = new QuoteService()
